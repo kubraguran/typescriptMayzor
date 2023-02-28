cea33420b7f8b27386c7bf6e777ad28d8fae3985
Kubra Guran <81765398+kubraguran@users.noreply.github.com>
2023-02-28 13:11:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignIn from './components/SignIn';
+import UsersTable from './components/UsersTable';
+import SingleUser from './components/SingleUser';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import SignUp from './components/SignUp';
+import MainPage from './components/MainPage';
+import ErrorPage from './components/ErrorPage';
 
-function App() {
+
+const App: React.FC = () => {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     
+          <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<SignIn email={''} />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="userstable" element={<UsersTable />} />
+            <Route path = "mainpage" element = {<MainPage />} />
+            <Route path = "*" element = {<ErrorPage />} />
+          </Routes>
+
+        </BrowserRouter>
+      
     </div>
   );
-}
+
+};
 
 export default App;
