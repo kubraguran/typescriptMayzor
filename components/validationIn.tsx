4382ef25d18ec95f3d146e43c59ec3d9de2c9316
Kubra Guran <81765398+kubraguran@users.noreply.github.com>
2023-02-28 13:05:48 +0100

    Delete components directory

--- components/validationIn.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import * as yup from 'yup';
-  
-const validation = yup.object({
-    email: yup.string().required('required field').email(),
-  });
-   
-  export default validation;
-  
